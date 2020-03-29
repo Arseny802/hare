3e2457f29699ab4e8a527ef13a3d9dc34040f409
Arseny802 <Arseny802@yandex.ru>
2020-03-29 10:49:38 +0300

    Two libraries for logging diagnostic

---
+++ src/sub_example_so/Worker.cpp
@@ -0,0 +1,47 @@
+//
+// Created by arseny on 28/03/2020.
+//
+
+#include <src/logging/hare.h>
+#include "Worker.h"
+
+namespace sub_example_so {
+
+const int Worker::kBytesToReserve = 128;
+
+Worker::Worker() {
+  hare::debug("Called Worker::Worker().");
+  hare::debug("Allocating {} bytes of memory...", kBytesToReserve);
+  some_data_ = new uint8_t[kBytesToReserve];
+  for (int index = 0; index < kBytesToReserve; ++index) {
+    some_data_[index] = (index * index / 2);
+  }
+  hare::trace("Allocating {} bytes of memory OK.", kBytesToReserve);
+}
+
+Worker::~Worker() {
+  hare::debug("Worker::~Worker() called. Deleting some_data_");
+  delete some_data_;
+}
+
+void Worker::SomeSetter(const std::string_view value) noexcept {
+  hare::trace("Worker::SomeSetter() called. Value is '{}'.", value);
+  some_string_ = value.data();
+}
+
+std::string Worker::SomeGetter() const noexcept {
+  hare::trace("Worker::SomeSetter() called. Value is '{}'.", some_string_);
+  return some_string_;
+}
+
+//template <class T>
+bool Worker::VectorHasRepetitions(const std::vector<int> &data) const noexcept {
+  hare::debug("Worker::SomeSetter() called.");
+
+  for (int element : data) {
+    hare::trace("Got elevent '{}' in data.", element);
+  }
+  return false;
+}
+
+}
