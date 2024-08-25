1a41f7c197a9e8724d01743d624c8670fd14df85
Arseny Ravin <Arseny802@yandex.ru>
2024-08-25 23:45:31 +0300

    feat: Add documentation and improve worker class functionality.

--- example/static_library_example/include/static_library_example/worker.h
+++ example/static_library_example/include/static_library_example/worker.h
@@ -4,25 +4,50 @@
 #include <vector>
 
 namespace static_library_example {
+  /**
+   * @brief A simple worker class that encapsulates some functionality.
+   *
+   * This class provides methods for performing tasks and handling data.
+   */
   class worker {
 public:
-    ///
+    /**
+     * Constructor for worker.
+     *
+     * @brief A brief description of what this class does.
+     */
     worker();
-    ///
+    /**
+     * Destructor for the worker. Frees any resources allocated by the
+     * constructor or during the lifetime of the object.
+     *
+     * @post No external resources will be held.
+     */
     ~worker();
-
-    ///
-    /// \param data
-    /// \return
+    /**
+     * Checks if a given vector of integers has any repetitions.
+     *
+     * @param data The vector to check for repetition.
+     * @return Whether or not the vector contains any repeated values.
+     */
     [[nodiscard]] static bool vector_has_repetitions(const std::vector<int>& data) noexcept;
-    ///
-    /// \return
+    /**
+     * Retrieves some value from this worker instance.
+     *
+     * @return The retrieved value.
+     */
     [[nodiscard]] std::string some_getter() const noexcept;
-    ///
-    /// \param value
+    /**
+     * Sets a value in the worker instance.
+     *
+     * @param value The new value to set.
+     */
     void some_setter(std::string_view value) noexcept;
 
-    static const int k_bytes_to_reserve_;
+    /**
+     * A constant that specifies how many bytes should be reserved for certain operations.
+     */
+    static const int k_bytes_to_reserve = 1024;
 
 private:
     std::string some_string_{};
